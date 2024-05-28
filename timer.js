0f755d9962d37a517a69c67d584140c56ed80b13
ker <ker10epere@gmail.com>
2024-05-28 22:09:03 +0800

    timer add done notif

--- timer.js
+++ timer.js
@@ -1,8 +1,11 @@
 let counter = 0
 const counterSecondsDuration = process.env.npm_config_counter_sec_duration
-console.log(`> Start ${counterSecondsDuration}`);
-let intervalId = setInterval(()=> {
+console.log(`> Start ${counterSecondsDuration}`)
+let intervalId = setInterval(() => {
   counter += 1
-  console.log(counter);
-if(counter == counterSecondsDuration) clearInterval(intervalId) 
-},1000)
+  console.log(counter)
+  if (counter == counterSecondsDuration) {
+    clearInterval(intervalId)
+    console.log("> Done!")
+  }
+}, 1000)
