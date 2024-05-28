f419a1d3728130e434237c08e845515e6cc8badb
ker <ker10epere@gmail.com>
2024-05-28 22:06:36 +0800

    trim and optimize

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require("fs")
+const path = require("path")
 
-const inputText = fs.readFileSync(path.join(__dirname, 'input.txt'),'utf8')
+const curPath = (file) => path.join(__dirname, file)
+const inputText = fs
+  .readFileSync(path.join(__dirname, "input.txt"), "utf8")
+  .split("\n")
+  .map((line) => line.trim())
 
-const urls = inputText.split('\n').filter(v=> v.includes('https')).join("\n")
-const output = inputText.split('\n').filter(v=> !v.includes('https')).join("\n")
+const urls = inputText.filter((v) => v.includes("https")).join("\n")
+const output = inputText.filter((v) => !v.includes("https")).join("\n")
 
-fs.writeFileSync(path.join(__dirname,'urls.txt'),urls,'utf8' )
-fs.writeFileSync(path.join(__dirname,'output.txt'),output,'utf8' )
-console.log('Done!')
+fs.writeFileSync(curPath("urls.txt"), urls, "utf8")
+fs.writeFileSync(curPath("output.txt"), output, "utf8")
+console.log("Done!")
